a2280aa269da2cb8dd69b9c71cd9085f34940b0c
Alireza Toghyiani <mr.toghyiani@icloud.com>
2020-08-02 14:01:25 +0430

    create project

---
+++ GithubUserInfo/GithubUserInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUserInfoApp.swift
+//  GithubUserInfo
+//
+//  Created by ali on 8/2/20.
+//
+
+import SwiftUI
+
+@main
+struct GithubUserInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
